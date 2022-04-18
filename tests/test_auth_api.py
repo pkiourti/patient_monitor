456839f5f5e73151f1b83f40afd950e94ac95de1
Penny <pkiourti@gmail.com>
2022-04-18 16:26:20 -0400

    add delay for tests

--- tests/test_auth_api.py
+++ tests/test_auth_api.py
@@ -1,8 +1,10 @@
 import requests
+import time
 
 BASE = "http://127.0.0.1:5000/"
 
 def test_auth_api():
+    time.sleep(2)
     # test post request
     json_data = {}
     json_data['email'] = "admin"
